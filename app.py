1a2704f0710a1d8d331addb14572528b7efdd91b
Closertech001 <danielajayi842@gmail.com>
2025-05-24 20:03:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ abbreviations = {
     "msg": "message", "doc": "document", "d": "the", "yr": "year", "sem": "semester",
     "dept": "department", "admsn": "admission", "cresnt": "crescent", "uni": "university",
     "clg": "college", "sch": "school", "info": "information", "l": "level", "CSC": "Computer Science",
-    "ECO": "Economics with Operations Research", "PHY": "Physics", "STA": "Statistics"
+    "ECO": "Economics with Operations Research", "PHY": "Physics", "STAT": "Statistics"
 }
 
 # Department mapping
