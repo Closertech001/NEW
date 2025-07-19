ee031033d4a3798b525ec55eb58b8c3adab8cb45
Closertech001 <danielajayi842@gmail.com>
2025-07-19 10:24:44 -0500

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,26 @@
+# utils/constants.py
+
+abbreviations = {
+    "u": "you", "r": "are", "ur": "your", "ow": "how", "pls": "please", "plz": "please",
+    "tmrw": "tomorrow", "cn": "can", "wat": "what", "cud": "could", "shud": "should",
+    "wud": "would", "abt": "about", "bcz": "because", "btw": "between", "asap": "as soon as possible",
+    "idk": "i don't know", "imo": "in my opinion", "msg": "message", "doc": "document", "d": "the",
+    "yr": "year", "sem": "semester", "dept": "department", "admsn": "admission",
+    "cresnt": "crescent", "uni": "university", "clg": "college", "sch": "school",
+    "info": "information", "l": "level", "CSC": "Computer Science", "ECO": "Economics with Operations Research",
+    "PHY": "Physics", "STAT": "Statistics", "1st": "First", "2nd": "Second"
+}
+
+department_map = {
+    "GST": "General Studies", "MTH": "Mathematics", "PHY": "Physics", "STA": "Statistics",
+    "COS": "Computer Science", "CUAB-CSC": "Computer Science", "CSC": "Computer Science",
+    "IFT": "Computer Science", "SEN": "Software Engineering", "ENT": "Entrepreneurship",
+    "CYB": "Cybersecurity", "ICT": "Information and Communication Technology",
+    "DTS": "Data Science", "CUAB-CPS": "Computer Science", "CUAB-ECO": "Economics with Operations Research",
+    "ECO": "Economics with Operations Research", "SSC": "Social Sciences", "CUAB-BCO": "Economics with Operations Research",
+    "LIB": "Library Studies", "LAW": "Law (BACOLAW)", "GNS": "General Studies", "ENG": "English",
+    "SOS": "Sociology", "PIS": "Political Science", "CPS": "Computer Science",
+    "LPI": "Law (BACOLAW)", "ICL": "Law (BACOLAW)", "LPB": "Law (BACOLAW)", "TPT": "Law (BACOLAW)",
+    "FAC": "Agricultural Sciences", "ANA": "Anatomy", "BIO": "Biological Sciences",
+    "CHM": "Chemical Sciences", "CUAB-BCH": "Biochemistry", "CUAB": "Crescent University - General"
+}
