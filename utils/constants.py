d6b232618aca771ec03f5bf15b84527897ee0f99
Closertech001 <danielajayi842@gmail.com>
2025-07-19 10:41:15 -0500

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,16 +1,38 @@
 # utils/constants.py
 
 abbreviations = {
-    "u": "you", "r": "are", "ur": "your", "ow": "how", "pls": "please", "plz": "please",
-    "tmrw": "tomorrow", "cn": "can", "wat": "what", "cud": "could", "shud": "should",
-    "wud": "would", "abt": "about", "bcz": "because", "btw": "between", "asap": "as soon as possible",
-    "idk": "i don't know", "imo": "in my opinion", "msg": "message", "doc": "document", "d": "the",
-    "yr": "year", "sem": "semester", "dept": "department", "admsn": "admission",
-    "cresnt": "crescent", "uni": "university", "clg": "college", "sch": "school",
-    "info": "information", "l": "level", "CSC": "Computer Science", "ECO": "Economics with Operations Research",
-    "PHY": "Physics", "STAT": "Statistics", "1st": "First", "2nd": "Second"
+    "u": "you", "r": "are", "ur": "your", "cn": "can", "cud": "could",
+    "shud": "should", "wud": "would", "abt": "about", "bcz": "because",
+    "plz": "please", "pls": "please", "tmrw": "tomorrow", "wat": "what",
+    "wats": "what is", "info": "information", "yr": "year", "sem": "semester",
+    "admsn": "admission", "clg": "college", "sch": "school", "uni": "university",
+    "cresnt": "crescent", "l": "level", "d": "the", "msg": "message",
+    "idk": "i don't know", "imo": "in my opinion", "asap": "as soon as possible",
+    "dept": "department", "reg": "registration", "fee": "fees", "pg": "postgraduate",
+    "app": "application", "req": "requirement", "nd": "national diploma",
+    "a-level": "advanced level", "alevel": "advanced level", "2nd": "second",
+    "1st": "first", "nxt": "next", "prev": "previous", "exp": "experience",
+    "CSC": "department of Computer Science", "Mass comm": "department of Mass Communication",
+    "law": "department of law", "Acc": "department of Accounting"
 }
 
+synonyms = {
+    "lecturers": "academic staff", "professors": "academic staff",
+    "teachers": "academic staff", "instructors": "academic staff",
+    "tutors": "academic staff", "staff members": "staff",
+    "head": "dean", "hod": "head of department", "dept": "department",
+    "school": "university", "college": "faculty", "course": "subject",
+    "class": "course", "subject": "course", "unit": "credit",
+    "credit unit": "unit", "course load": "unit", "non teaching": "non-academic",
+    "admin worker": "non-academic staff", "support staff": "non-academic staff",
+    "clerk": "non-academic staff", "receptionist": "non-academic staff",
+    "secretary": "non-academic staff", "tech staff": "technical staff",
+    "hostel": "accommodation", "lodging": "accommodation", "room": "accommodation",
+    "school fees": "tuition", "acceptance fee": "admission fee", "fees": "tuition",
+    "enrol": "apply", "join": "apply", "sign up": "apply", "admit": "apply",
+    "requirement": "criteria", "conditions": "criteria", "needed": "required",
+    "needed for": "required for", "who handles": "who manages"
+}
 department_map = {
     "GST": "General Studies", "MTH": "Mathematics", "PHY": "Physics", "STA": "Statistics",
     "COS": "Computer Science", "CUAB-CSC": "Computer Science", "CSC": "Computer Science",
